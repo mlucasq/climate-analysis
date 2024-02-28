96c8db7e6da271dc28d326119d32654b8192cf9c
mlucasq <mlucasq@hotmail.com>
2024-02-28 12:57:51 +0000

    Add rainfall placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -26,3 +26,4 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+# TODO(mlucasq): Add call to process rainfall
